d8c93e75b2b9a44608807c348df0983ee35511f1
Ibrahim Yahia <iyahia@zenith.com.au>
2020-11-03 07:20:29 +0200

    Add a search bar, which allows users to search for movies

--- src/app/components/movie-list/movie-list.component.ts
+++ src/app/components/movie-list/movie-list.component.ts
@@ -3,6 +3,8 @@ import {MovieApiService} from '../../services/api/movie-api.service';
 import {Page} from '../../model/page.model';
 import {Movie} from '../../model/movie.model';
 import {ConfigApiService} from '../../services/api/config-api.service';
+import {Observable} from "rxjs";
+import {SearchApiService} from "../../services/api/search-api.service";
 
 @Component({
   selector: 'app-movie-list',
@@ -14,24 +16,40 @@ export class MovieListComponent implements OnInit {
   moviesPage: Page<Movie>;
   posterImageBaseUrl: string;
 
-  constructor(private movieApi: MovieApiService, private config: ConfigApiService) {
+  constructor(private config: ConfigApiService,
+              private movieApi: MovieApiService,
+              private searchApi: SearchApiService) {
   }
 
   ngOnInit(): void {
     this.posterImageBaseUrl = this.config.getPosterImageBaseUrl();
-    this.movieApi.getUpcomingMovies().subscribe((moviesPage) => this.moviesPage = moviesPage);
-  }
-
-  getMoviesPage(pageNumber: number): void {
-    this.movieApi.getUpcomingMovies(pageNumber).subscribe((moviesPage) => this.moviesPage = moviesPage);
+    this.getMoviesPage();
   }
 
   searchMovies($event: any): void {
-    if (!this.searchTerm) {
+    if (this.searchTerm === $event.target.value) {
       return;
     }
-    console.log($event.target.value);
-    console.log(this.searchTerm);
+    this.searchTerm = $event.target.value;
+    this.getMoviesPage();
+  }
+
+  getMoviesPage(pageNumber?: number): void {
+    this.getApi(pageNumber).subscribe((moviesPage) => {
+      this.moviesPage = moviesPage;
+      window.scroll({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+      });
+    });
+  }
+
+  private getApi(pageNumber: number): Observable<Page<Movie>> {
+    if (this.searchTerm) {
+      return this.searchApi.searchMovies(this.searchTerm, pageNumber);
+    }
+    return this.movieApi.getUpcomingMovies(pageNumber);
   }
 
   showMovieDetails(): void {
--- src/app/services/api/search-api.service.ts
+++ src/app/services/api/search-api.service.ts
@@ -1,9 +1,25 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpParams} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {HttpService} from '../common/http.service';
+import {Page} from '../../model/page.model';
+import {Movie} from '../../model/movie.model';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchApiService {
+  private readonly apiController: string = 'search/movie';
 
-  constructor() { }
+  constructor(private api: HttpService) {
+  }
+
+  searchMovies(searchTerm: string, pageNumber?: number): Observable<Page<Movie>> {
+    pageNumber = pageNumber && pageNumber > 0 ? pageNumber : 1;
+    const params = new HttpParams()
+      .set('query', searchTerm)
+      .set('page', pageNumber.toString());
+    return this.api.get(this.apiController, params);
+  }
 }
